88b7f7b9a432c3be8239bde396e96db941de5e98
George J. Wilkie <george.wilkie@gmail.com>
2016-02-15 12:38:02 +0100

    Removed dependence on Cubature

--- julia/Genquad.jl
+++ julia/Genquad.jl
@@ -1,5 +1,4 @@
 module Genquad
-using Cubature
 using Base.Test
 
 export genquadrules
@@ -49,25 +48,25 @@ function genquadrules(wgtfcn::Function,N::Integer,endpts::Array{Float64,1})
       return wgtfcn(x)*y*y*x
    end
 
-   moment1, dummy = pquad(wgt_x,endpts...;reltol=epsfac*epsilon)
-   moment2, dummy = pquad(wgtfcn,endpts...;reltol=epsfac*epsilon)
+   moment1, dummy = quadgk(wgt_x,endpts...;reltol=epsfac*epsilon)
+   moment2, dummy = quadgk(wgtfcn,endpts...;reltol=epsfac*epsilon)
    b[1] = moment2
 #   b[1] = 1.0
    a[1] = moment1/moment2
    order = 1
-   moment1, dummy = pquad(poly_sqx,endpts...;reltol=epsfac*epsilon)
-   moment2, dummy = pquad(poly_sq,endpts...;reltol=epsfac*epsilon)
+   moment1, dummy = quadgk(poly_sqx,endpts...;reltol=epsfac*epsilon)
+   moment2, dummy = quadgk(poly_sq,endpts...;reltol=epsfac*epsilon)
    a[2] = moment1/moment2
    order = 0
-   moment1, dummy = pquad(poly_sq,endpts...;reltol=epsfac*epsilon)
+   moment1, dummy = quadgk(poly_sq,endpts...;reltol=epsfac*epsilon)
    b[2] = moment2/moment1
    for n = 3:N
       order = n-1
-      moment1, dummy = pquad(poly_sqx,endpts...;reltol=epsfac*epsilon)
-      moment2, dummy = pquad(poly_sq,endpts...;reltol=epsfac*epsilon)
+      moment1, dummy = quadgk(poly_sqx,endpts...;reltol=epsfac*epsilon)
+      moment2, dummy = quadgk(poly_sq,endpts...;reltol=epsfac*epsilon)
       a[n] = moment1/moment2
       order = n-2
-      moment1, dummy = pquad(poly_sq,endpts...;reltol=epsfac*epsilon)
+      moment1, dummy = quadgk(poly_sq,endpts...;reltol=epsfac*epsilon)
       b[n] = moment2/moment1
    end
 
@@ -97,51 +96,6 @@ function genquadrules(wgtfcn::Function,N::Integer,endpts::Array{Float64,1})
    end
 
    return abscissae, intwgts
-end
-
-"Acts as a wrapper for Cubature's pquadrature, with the possibility of infinite or semiinfinite domains"
-function pquad(f::Function,endpts...;reltol=1.0e-8)
-
-#   f_y = Function
-   endpts_y = [0.0,0.0]
-   # If user specifies any singularities in the domain, just use quadgk
-   if length(endpts) > 2
-      return Base.quadgk(f,endpts...;reltol=reltol)
-   else
-      if isinf(endpts[1])
-         if isinf(endpts[2])
-            # If infinite:  
-            f_y(x) = f(x/(1.0-x^2))*(1+x^2)/(1.0-x^2)^2 
-            endpts_y = [-1.0,1.0]
-         else  
-            # If semi-infinite:  
-            f_y(x) = -f(endpts[2]-x/(1.0-x))/(1.0-x)^2
-            endpts_y = [0.0,1.0]
-         end
-      else
-         if isinf(endpts[2])
-#println("Got here")
-            # If semi-infinite:  
-            f_y(x) = f(endpts[1] + x/(1.0-x))/(1.0-x)^2
-            endpts_y = [0.0,1.0]
-         else  
-            # If both bounds are finite, do no transformation
-            f_y(x) = f(x)
-            endpts_y = endpts
-         end
-      end
-      if (isinf(f_y(endpts_y[1])) || isinf(f_y(endpts_y[2])))  || (isnan(f_y(endpts_y[1])) || isnan(f_y(endpts_y[2]))) 
-         # If endpoints cannot be evaluated, just use quadgk
-         return Base.quadgk(f,endpts...;reltol=reltol)
-      else
-         return Cubature.pquadrature(f_y,endpts_y[1],endpts_y[2];reltol=reltol)
-      end
-   end
-
-   # Transformed integrand
-   function fy(x) 
-      return f(a*x+b)
-   end
 end
 
 function test()
